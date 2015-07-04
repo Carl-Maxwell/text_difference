44a9342d308d6e318bb8a742180480c5af18a883
Carl Maxwell <carl.maxwell@gmail.com>
2015-07-04 12:31:50 -0700

    ok, so, _now_ it solves that super simple case

--- diff.rb
+++ diff.rb
@@ -9,23 +9,18 @@ class Diff
     a.each.with_index do |a_line, a_line_number|
       b.find.with_index do |b_line, b_line_number|
         if a_line == b_line
-          unless unchanged.empty?
-            combination = []
-            lines = unchanged.last[0]+1...a_line_number
-
-            lines.to_a.length.times do |i|
-              combination << [lines.to_a[i], a[lines][i] ]
-            end
-
-            removed.concat( combination )
-          end
-
           unchanged << [a_line_number, a_line]
           b = b[0...b_line_number] + b[b_line_number+1...b.length]
           next b_line
         end
 
         false
+      end
+    end
+
+    a.each.with_index do |a_line, a_line_number|
+      unless unchanged.include?([a_line_number, a_line])
+        removed << [a_line_number, a_line]
       end
     end
 
