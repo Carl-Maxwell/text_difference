1846465cb935a6d1a7d1ca6639ddaf408d6ed645
Carl Maxwell <carl.maxwell@gmail.com>
2015-07-04 18:15:21 -0700

    added diff_files and diff_strings methods
    and a module to store the information about line numbers so I can make the diff code more succinct
    
    also added a slightly more complex failing test case

--- diff.rb
+++ diff.rb
@@ -1,8 +1,50 @@
 
 class Diff
-  def self.diff(a, b)
+  def self.diff_strings(a, b)
     a, b = a.split("\n"), b.split("\n")
 
+    [a, b].each do |x|
+      x.map!.with_index do |str, number|
+        str.extend(Line)
+        str.line_number = number
+
+        str
+      end
+    end
+
+    self.diff(a, b)
+  end
+
+  def self.diff_files(a, b)
+    # a_file, b_file = File.open(a_file), File.open(b_file)
+
+    # loop over each line & map to Line
+
+    a, b = [a, b].map do |a_or_b|
+      lines = []
+
+      File.open(a_or_b) do |file|
+        until file.eof?
+          str = file.gets.chomp
+          str.extend(Line)
+          str.line_number = lines.length
+          lines << str
+        end
+      end
+
+      lines
+    end
+
+    self.diff(a, b)
+  end
+
+  module Line
+    attr_accessor :line_number
+  end
+
+  protected
+
+  def self.diff(a, b)
     removed = []
     unchanged = []
 
@@ -24,6 +66,6 @@ class Diff
       end
     end
 
-    [b, removed]
+    {added: b, removed: removed}
   end
 end
